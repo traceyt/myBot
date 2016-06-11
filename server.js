e2406948407abe05de030453f5dc42073ad70e16
Tracey Trewin <tracey@trewin.com>
2016-06-11 10:06:28 -0700

    update with more logging info

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.use(restify.bodyParser());
 
 // log the url posted and then post the message
 server.use(function(req, res, next) {
-    console.log(req.method + ' ' + req.url);
+    console.log(req.method + ' ' + req.url + ' ' + req.username + ' ' + req.body.text);
     next();
 });
 
