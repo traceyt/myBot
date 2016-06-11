56d4882dc5b8fbdb3517dbadd5f5ec86d2eec931
Tracey Trewin <tracey@trewin.com>
2016-06-10 18:10:42 -0700

    update before pushing to github

--- server.js
+++ server.js
@@ -5,20 +5,25 @@ var restify = require("restify");
 var builder = require("botbuilder");
 
 var bot = new builder.BotConnectorBot({ appID: botAppID, appSecret: botAppSecret });
-bot.add("/", function(session) { session.send("Hello world!!!"); });
+bot.add("/", hello);
 
 var server = restify.createServer({ name:"Bot Server" });
-// server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-// server.listen(process.env.port || 3978, function() { console.log("%s listening to %s", server.name, server.url)});
 
+
+// log the url posted and then post the message
 server.use(function(req, res, next) {
     console.log(req.method + ' ' + req.url);
+    next();
 });
-
-server.use(restify.bodyParser());
 
 server.post('api/messages', bot.verifyBotFramework(), bot.listen());
 
 server.listen(process.env.port || 3978, function() { 
     console.log('api listening at %s', server.url)
 });
+
+
+
+function hello(session) {
+  session.send("Hello world!!!");
+}
